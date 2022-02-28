20f9dfeacdaad262124df2c457a6677d34c4573f
Patrick Sparks <pto1026@gmail.com>
2022-02-28 02:21:14 -0700

    implemented react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Create from './Create/Create'
+import Edit from './Edit/Edit'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='create' element={<Create />} />
+                <Route path='edit' element={<Edit />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
     document.querySelector('#root')
 )
