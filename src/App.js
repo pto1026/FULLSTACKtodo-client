8a542858c35b9463f30bef221df569c3be2a641d
Patrick <pto1026@gmail.com>
2022-02-28 04:45:58 -0700

    completed some work on the main page, ran into issue don't have postman on this computer, so ive decided to just do the create page now so I can test it.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import Navbar from './components/Navbar/Navbar'
 const App = () => {
     useEffect(() => {
         document.title = 'Todoly | The Todo App'
-    })
+        const retrievePosts = async () => {
+            const response = await fetch('http://localhost:5000/api/todos')
+            const data = await response.json()
+
+            return data;
+        }
+
+        retrievePosts()
+    }, [])
     return (
         <div className='page-container'>
            <Navbar />
