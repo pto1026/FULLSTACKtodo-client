33eb8e2cf11b9e561f31e610c44bd7f7d133793e
Patrick <pto1026@gmail.com>
2022-02-28 05:54:57 -0700

    completed some work on the create page

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Navbar from "../components/Navbar/Navbar";
 
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [status, setStatus] = useState('')
+    const [priority, setPriority] = useState('')
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const todo = {
+            title,
+            description,
+            status,
+            priority,
+        }
+        await fetch('http://localhost:5000/api/todos', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: todo
+        })
+
+        setTitle('')
+        setDescription('')
+        setStatus('')
+        setPriority('')
+    }
+
+    return (
+        <div>
+            <Navbar/>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label for='title'>Title: </label>
+                    <input type="text" name='title' value={title} onChange={({target}) => setTitle(target.value)}/>
+                </div>
+                <div>
+                    <label for='description'>Description: </label>
+                    <input type="text" name='description' value={description} onChange={({target}) => setDescription(target.value)} />
+                </div>
+                <div>
+                    <label for='status'>Status: </label>
+                    ###
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Create
