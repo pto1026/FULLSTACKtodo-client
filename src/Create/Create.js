af4d28dfd4cf59d909e865a7892ced38a3bd9141
Patrick Sparks <pto1026@gmail.com>
2022-03-03 12:55:37 -0700

    implemented navbar styling

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -38,55 +38,63 @@ const Create = () => {
     }
 
     return (
-        <div>
+        <div className="container">
             <Navbar/>
-            <form onSubmit={handleSubmit} className='form-container'>
-                <div className='form-element'>
-                    <label htmlFor="title">Title: </label>
-                    <input type="text" name="title" id="title" value={title}
-                           onChange={({target}) => setTitle(target.value)}/>
-                </div>
-                <div className='form-element'>
-                    <label htmlFor="description">Description: </label>
-                    <input type="text" id="description" name="description" value={description}
-                           onChange={({target}) => setDescription(target.value)}/>
-                </div>
-                <div className='form-element'>
-                    <p>
-                        Status:
-                    </p>
-                    <div className='status'>
-                        <input type="radio" id="statusChoice1"
-                               name="status" value="Pending" checked={status === 'Pending'} onChange={({target}) => setStatus(target.value)}/>
-                        <label htmlFor="statusChoice1">Pending</label>
-                        <input type="radio" id="statusChoice2"
-                               name="status" value="In Progress" checked={status === 'In Progress'} onChange={({target}) => setStatus(target.value)}/>
-                        <label htmlFor="statusChoice2">In Progress</label>
-                        <input type="radio" id="statusChoice3"
-                               name="status" value="Complete" checked={status === 'Complete'} onChange={({target}) => setStatus(target.value)}/>
-                        <label htmlFor="statusChoice3">Complete</label>
+            <div className="main-container">
+                <form onSubmit={handleSubmit} className="form-container">
+                    <div className="form-element">
+                        <label htmlFor="title">Title: </label>
+                        <input type="text" name="title" id="title" value={title}
+                               onChange={({target}) => setTitle(target.value)}/>
                     </div>
-                </div>
-                <div className='form-element'>
-                    <p>
-                        Priority:
-                    </p>
-                    <div className='priority'>
-                        <input type="radio" id="priority1"
-                               name="priority" value="Low" checked={priority === 'Low'} onChange={({target}) => setPriority(target.value)}/>
-                        <label htmlFor="priority1">Low</label>
-                        <input type="radio" id="priority2"
-                               name="priority" value="Med" checked={priority === 'Med'} onChange={({target}) => setPriority(target.value)}/>
-                        <label htmlFor="priority2">Med</label>
-                        <input type="radio" id="priority3"
-                               name="priority" value="High" checked={priority === 'High'} onChange={({target}) => setPriority(target.value)}/>
-                        <label htmlFor="priority3">High</label>
+                    <div className="form-element">
+                        <label htmlFor="description">Description: </label>
+                        <input type="text" id="description" name="description" value={description}
+                               onChange={({target}) => setDescription(target.value)}/>
                     </div>
-                </div>
-                <div className='form-element submit-button'>
-                    <button type='submit'>Save</button>
-                </div>
-            </form>
+                    <div className="form-element">
+                        <p>
+                            Status:
+                        </p>
+                        <div className="status">
+                            <input type="radio" id="statusChoice1"
+                                   name="status" value="Pending" checked={status === 'Pending'}
+                                   onChange={({target}) => setStatus(target.value)}/>
+                            <label htmlFor="statusChoice1">Pending</label>
+                            <input type="radio" id="statusChoice2"
+                                   name="status" value="In Progress" checked={status === 'In Progress'}
+                                   onChange={({target}) => setStatus(target.value)}/>
+                            <label htmlFor="statusChoice2">In Progress</label>
+                            <input type="radio" id="statusChoice3"
+                                   name="status" value="Complete" checked={status === 'Complete'}
+                                   onChange={({target}) => setStatus(target.value)}/>
+                            <label htmlFor="statusChoice3">Complete</label>
+                        </div>
+                    </div>
+                    <div className="form-element">
+                        <p>
+                            Priority:
+                        </p>
+                        <div className="priority">
+                            <input type="radio" id="priority1"
+                                   name="priority" value="Low" checked={priority === 'Low'}
+                                   onChange={({target}) => setPriority(target.value)}/>
+                            <label htmlFor="priority1">Low</label>
+                            <input type="radio" id="priority2"
+                                   name="priority" value="Med" checked={priority === 'Med'}
+                                   onChange={({target}) => setPriority(target.value)}/>
+                            <label htmlFor="priority2">Med</label>
+                            <input type="radio" id="priority3"
+                                   name="priority" value="High" checked={priority === 'High'}
+                                   onChange={({target}) => setPriority(target.value)}/>
+                            <label htmlFor="priority3">High</label>
+                        </div>
+                    </div>
+                    <div className="form-element submit-button">
+                        <button type="submit">Save</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
