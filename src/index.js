062c3103ba2e61ba08029dfeee20b95082f050e6
Patrick Sparks <pto1026@gmail.com>
2022-03-03 12:14:05 -0700

    initial commit, removed styling added new colors and font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App'
 import Create from './Create/Create'
 import Edit from './Edit/Edit'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './styles.css'
 
 ReactDOM.render(
     <React.StrictMode>
